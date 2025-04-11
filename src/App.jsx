670b5dca90c680f6ce44da9a5e8c2d3199d91f7f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-11 15:08:35 +0200

    feat: complete milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import axios from 'axios';
+
+
 function App() {
+  const endpoint = 'https://freetestapi.com/api/v1/actresses';
+
+  function fetchActresses() {
+    axios.get(endpoint)
+      .then((res) => console.log(res.data));
+  }
+  fetchActresses();
   return (
     <>
     </>

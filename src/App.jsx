753b8016eaf874d5411225d99a423ac11a2ad375
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-11 15:30:33 +0200

    feat: complete milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
 function App() {
+  const [actresses, setActresses] = useState([]);
+
   const endpoint = 'https://www.freetestapi.com/api/v1/actresses';
 
   function fetchActresses() {
     axios.get(endpoint)
-      .then((res) => console.log(res.data));
+      .then((res) => {
+        setActresses(res.data);
+      });
   }
-  fetchActresses();
+
+  useEffect(fetchActresses, []);
   return (
     <>
       <h1>Attrici</h1>
-      <div className='card'>
-        <h2>Nome</h2>
-        <p>Anno nascita</p>
-        <p>Nazionalità</p>
-        <p>Bio</p>
-        <p>Premi</p>
-        <p>Film Famosi in cui ha partecipato</p>
-        <img src="#" alt="" />
+      {actresses.map(actress =>
+        <div className='card' key={actress.id}>
+          <h2>{actress.name}</h2>
+          <img src={actress.image} alt={actress.name} />
+          <p>Anno nascita: {actress.birth_year}</p>
+          <p>Nazionalità: {actress.nationality}</p>
+          <p>Bio: {actress.biography}</p>
+          <p>Premi: {actress.awards}</p>
+          <p>Film Famosi in cui ha partecipato: {actress.most_famous_movies.join(', ')}</p>
+        </div>
+      )}
 
-      </div>
     </>
   );
 }
